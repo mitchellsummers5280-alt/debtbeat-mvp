dd498be7813803b6b56060b1bbfebb1feb835ab8
Brian Summers <briansummers@Brians-MacBook-Pro.local>
2025-12-05 21:47:31 -0500

    Add Link import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";        // ← ADD THIS LINE
 import "./globals.css";
 
 import AuthProvider from "./AuthProvider";
